c4c1e9699d45ed65d6be93691d407c3a4120dd55
juanmartnez <juanmartnezzapata@outlook.es>
2017-11-25 17:30:50 -0600

    Primer Commit

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$('#barradenavegacion ul li a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            var t = this.hash;
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 1000, function () {
+                window.location.hash = t;
+            });
+        })
